db611fba14db6659d99554fe96d00735b16c4e19
wagnersouz4 <wagnersouz4@gmail.com>
2018-02-11 22:54:47 -0200

    day07

---
+++ 07 - Array Cardio Day 2/script.js
@@ -0,0 +1,39 @@
+// ## Array Cardio Day 2
+
+const people = [
+  { name: 'Wes', year: 1988 },
+  { name: 'Kait', year: 1986 },
+  { name: 'Irv', year: 1970 },
+  { name: 'Lux', year: 2015 }
+];
+
+const comments = [
+  { text: 'Love this!', id: 523423 },
+  { text: 'Super good', id: 823423 },
+  { text: 'You are the best', id: 2039842 },
+  { text: 'Ramen is my fav food ever', id: 123523 },
+  { text: 'Nice Nice Nice!', id: 542328 }
+];
+
+const isNineteenOrOlder = (person, year) => (year - person.year) >= 19;
+
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+const currentYear = (new Date()).getFullYear();
+const isSomeoneNineteenOrOlder = people.some(person => isNineteenOrOlder(person, currentYear));
+// Array.prototype.every() // is everyone 19 or older?
+const isEveryoneNineteenOrOlder  = people.every(person => isNineteenOrOlder(person, currentYear));
+
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+const comment = comments.find(comment => comment.id === 823423);
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
+const index = comments.findIndex(comment => comment.id === 823423);
+const newComments = [
+  ...comments.slice(0, index),
+  ...comments.slice(index + 1)
+];
