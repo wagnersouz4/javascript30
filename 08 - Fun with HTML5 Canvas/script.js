1821bacc622ba0c433e7e6543c0bb98e76771708
wagnersouz4 <wagnersouz4@gmail.com>
2018-02-12 23:47:53 -0200

    small fixes

---
+++ 08 - Fun with HTML5 Canvas/script.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = '#BADA55';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
