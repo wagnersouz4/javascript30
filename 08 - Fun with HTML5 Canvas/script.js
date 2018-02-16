87db90d9356a374da9d17b90f58bfabfcd900041
wagnersouz4 <wagnersouz4@gmail.com>
2018-02-15 23:56:36 -0200

    day 8

--- 08 - Fun with HTML5 Canvas/script.js
+++ 08 - Fun with HTML5 Canvas/script.js
@@ -8,25 +8,39 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round'; // how segments a joined
 ctx.lineCap = 'round'; // how the end points of every line are drawn
-ctx.lineWidth = 100;
+ctx.lineWidth = 500;
+ctx.globalCompositeOperation = 'source-out';
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
-
+let hue = 0;
+let direction = true;
 
 function draw(e) {
     if (!isDrawing) { return; }
-    console.log(e);
+
     ctx.beginPath();
     // start from
     ctx.moveTo(lastX, lastY);
+
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 
     // go to
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();
 
     [lastX, lastY] = [e.offsetX, e.offsetY];
+
+    hue++;
+
+    if (hue > 360) { hue = 0; }
+
+    if (ctx.lineWidth === 500 || ctx.lineWidth === 1) {
+        direction = !direction;
+    }
+
+    ctx.lineWidth = (direction) ? ctx.lineWidth + 1 : ctx.lineWidth - 1;
 }
 
 canvas.addEventListener('mousemove', draw);
